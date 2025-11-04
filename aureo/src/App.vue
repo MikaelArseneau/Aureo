9a2b21d018afb31b6597c619ffb9c79ecffcd1aa
Rafael-ADube <kostodube@gmail.com>
2025-11-04 12:28:08 -0500

    creation de l'app vue

---
+++ aureo/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import Allo from "./components/Allo.vue"
+  import Allo2 from "./components/Allo2.vue"
+  import Allo3 from "./components/Allo3.vue"
+</script>
+
+<template>
+  <allo />
+  <allo2 />
+  <allo3 />
+</template>
+
+<style scoped>
+
+</style>
---
+++ aureo/src/components/Allo2.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+ <h1>Aureo</h1>
+</template>
+
+<style scoped>
+
+</style>
