c00ebe90f1e2a570e03be2334f423ad963af8034
MikaelArseneau <mikael.arseneau@hotmail.ca>
2025-11-25 17:53:35 -0500

    page a propos

--- aureo/src/views/ProposView.vue
+++ aureo/src/views/ProposView.vue
@@ -1,1 +1,37 @@
-<template>da</template>
+<template>
+  <div class="container_propos">
+    <div class="titre">Aureo *</div>
+    <div class="texte">
+      Aureo est votre espace personnel dédié aux images qui vous inspirent.
+      Déposez, classez et retrouvez facilement vos illustrations favorites.
+      Pensé pour les créatifs, Aureo simplifie la sauvegarde visuelle et vous
+      aide à construire une bibliothèque d'inspirations unique, accessible
+      partout et à tout moment.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container_propos {
+  display: flex;
+  width: 90vw;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+  margin: 0 40px;
+}
+.titre {
+  color: #1a1a1a;
+  font-family: "switzer";
+  font-weight: 600;
+  text-align: left;
+  align-self: flex-start;
+  justify-self: start;
+  font-size: 100px;
+}
+
+.texte {
+  color: #1a1a1a;
+}
+</style>
